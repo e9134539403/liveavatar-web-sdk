783d5c163a1e575b4329c2d7f8968830715f1739
e9134539403 <155011561+e9134539403@users.noreply.github.com>
2025-12-23 19:23:43 +0700

    Update secrets.ts

--- apps/demo/app/api/secrets.ts
+++ apps/demo/app/api/secrets.ts
@@ -1,14 +1,14 @@
-export const API_KEY = "-";
+// Ключ LiveAvatar
+export const API_KEY = "27085cb7-3b64-406c-805d-6b1b2d51430b";
 export const API_URL = "https://api.liveavatar.com";
-export const AVATAR_ID = "dd73ea75-1218-4ef3-92ce-606d5f7fbc0a";
 
-// FULL MODE Customizations
-// Wayne's avatar voice and context
+// Ваш Аватар и База Знаний (Full Mode)
+export const AVATAR_ID = "dd73ea75-1218-4ef3-92ce-606d5f7fbc0a";
+export const CONTEXT_ID = "fc441781-575f-4604-9a00-315f41ae25c4";
 export const VOICE_ID = "c2527536-6d1f-4412-a643-53a3497dada9";
-export const CONTEXT_ID = "5b9dba8a-aa31-11f0-a6ee-066a7fa2e369";
+
 export const LANGUAGE = "en";
 
-// CUSTOM MODE Customizations
-export const ELEVENLABS_API_KEY = "YOUR_ELEVENLABS_API_KEY";
-export const OPENAI_API_KEY = "YOUR_OPENAI_API_KEY";
-
+// Оставляем пустыми
+export const OPENAI_API_KEY = "";
+export const ELEVENLABS_API_KEY = "";
